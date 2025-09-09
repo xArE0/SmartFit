59cc4e65fc15b78670c68b922a0fb50bcf787e83
Avishek Shrestha <abhishrestha987@gmail.com>
2025-09-09 19:39:45 +0545

    Session Manager

--- app/src/main/java/com/example/project_smartfit/MainActivity.kt
+++ app/src/main/java/com/example/project_smartfit/MainActivity.kt
@@ -6,17 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.*
 import androidx.compose.material3.*
+import androidx.compose.ui.platform.LocalContext
+import com.example.project_smartfit.ui.theme.SessionManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var screen by remember { mutableStateOf("signup") }
+            val context = LocalContext.current
+            var screen by remember {
+                mutableStateOf(
+                    if (SessionManager.getInstance(context).isLoggedIn()) "home" else "signup"
+                )
+            }
             when (screen) {
                 "signup" -> Signup(onSignupSuccess = { screen = "login" })
                 "login" -> Login(onLoginSuccess = { screen = "home" })
-                "home" -> Homepage()
+                "home" -> Homepage(onLogout = {
+                    SessionManager.getInstance(context).clearSession()
+                    screen = "login"
+                })
             }
         }
     }
